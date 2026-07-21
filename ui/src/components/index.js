e172c2adf53dfdc09a87190dca468818959059e0
Agustin26l <agustin.lopez2606@gmail.com>
2026-07-21 03:03:21 -0500

    Se agregan componentes para: -Seleccionar formato -Seleccionar carpeta (Corregir)

--- ui/src/components/index.js
+++ ui/src/components/index.js
@@ -1,3 +1,5 @@
 export { SidebarMenu } from "./sidebar-menu.js";
 export { UrlSearchBar } from "./url-search-bar.js";
 export { VideoPreviewCard } from "./video-preview-card.js";
+export { DownloadOptionsPanel } from "./download-options-panel.js";
+export { FolderPicker } from "./folder-picker.js";
