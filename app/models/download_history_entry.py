e030fabd020c2052f8fe65cc6c84a6a0032fea11
Agustin26l <agustin.lopez2606@gmail.com>
2026-07-31 20:13:52 -0500

    Se agrega servicio de historial y funcionalidades para crear un history.json

---
+++ app/models/download_history_entry.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+@dataclass(slots=True)
+class DownloadHistoryEntry:
+    id: str
+    url: str
+    title: str
+    uploader: str
+    thumbnail: str
+    output_directory: str
+    filename: str
+    extension: str
+    is_audio: bool
+    format_string: str
+    audio_codec: str | None
+    downloaded_at: str
